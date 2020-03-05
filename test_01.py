52be2a1dae3f4d3a60c72a781ee4e45d6366df06
noahzhy <noahzhy@gmail.com>
2020-03-05 17:25:23 +0900

    version:1.0.1

--- test_01.py
+++ test_01.py
@@ -1,2 +1,3 @@
 test_01
 _fix
+_updated
--- update.py
+++ update.py
@@ -4,27 +4,21 @@ import json
 from configparser import ConfigParser
 
 
-def check_version():
+config_file = 'config.ini'
+
+def get_newest_version():
     local_version, domain, update_ini = load_config(config_file='config.ini')
-    def get_newest_version():
-        try:
-            url = 'http://{}/{}'.format(domain, update_ini)
-            sess = requests.Session()
-            r = sess.post(url=url)
+    try:
+        url = 'http://{}/{}'.format(domain, update_ini)
+        sess = requests.Session()
+        r = sess.post(url=url)
 
-            if r.status_code == 200:
-                j = r.json()
-                return j['version']
+        if r.status_code == 200:
+            j = r.json()
+            return j['version']
 
-        except Exception as e:
-            print(e)
-            pass
-
-    newest = get_newest_version()
-    if (local_version < newest):
-        return True
-    else:
-        return False
+    except Exception as e:
+        print(e)
 
 
 def load_config(config_file):
@@ -52,6 +46,15 @@ def get_file_list(url):
     return file_list
 
 
+def update_or_not():
+    local_version, domain, update_ini = load_config(config_file='config.ini')
+    newest = get_newest_version()
+    if (local_version < newest):
+        return True
+    else:
+        return False
+
+
 def download_files(file_list):
     def download_big_file(domain, file_name):
         try:
@@ -61,28 +64,35 @@ def download_files(file_list):
                 for chunk in r.iter_content(chunk_size=1024):
                     if chunk:
                         pdf.write(chunk)
+            
+            return True
         except Exception as e:
             print(e)
+            return False
 
     file_counter = 0
     _, domain, _ = load_config(config_file='config.ini')
 
     for i in file_list:
         print(domain, i)
-        file_counter += 1
-        download_big_file(domain, i)
+        if download_big_file(domain, i):
+            file_counter += 1
 
     return True if file_counter == len(file_list) else False
 
 
 def run():
-    _, domain, file_list = load_config(config_file='config.ini')
-    api = 'http://{}/{}'.format(domain, file_list)
-    fl = get_file_list(api)
-    download_files(fl)
+    _, domain, file_list = load_config(config_file)
+    url = 'http://{}/{}'.format(domain, file_list)
+    fl = get_file_list(url)
+    if download_files(fl):
+        config = ConfigParser()
+        config.read(config_file)
+        config.set('update_config', 'version', get_newest_version())
+        with open(config_file, 'w') as cf:
+            config.write(cf)
 
 
 if __name__ == "__main__":
-    # run()
-    if check_version():
+    if update_or_not():
         run()
