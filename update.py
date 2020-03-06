fe9a3b58731ee3dac2c593141171bf05d3e0b2d1
noahzhy <noahzhy@gmail.com>
2020-03-07 05:46:24 +0900

    fix

--- update.py
+++ update.py
@@ -5,57 +5,42 @@ from configparser import ConfigParser
 
 
 config_file = 'config.ini'
-
-def get_newest_version():
-    local_version, domain, update_file = load_config(config_file)
-    try:
-        url = 'http://{}/{}'.format(domain, update_file)
-        sess = requests.Session()
-        r = sess.post(url=url)
-        if r.status_code == 200:
-            j = r.json()
-            return j['version']
-    except Exception as e:
-        print(e)
-        return local_version
+config = ConfigParser()
+config.read(config_file, encoding='UTF-8')
+local_version = config['update_config']['version']
+domain = config['update_config']['domain']
+update_file = config['update_config']['update_file']
 
 
-def load_config(config_file):
-    config = ConfigParser()
-    config.read(config_file, encoding='UTF-8')
-    version = config['update_config']['version']
-    domain = config['update_config']['domain']
-    update_file = config['update_config']['update_file']
-    return (version, domain, update_file)
+class Update():
+    def __init__(self):
+        version = local_version
+        file_list = []
+        try:
+            url = 'http://{}/{}'.format(domain, update_file)
+            sess = requests.Session()
+            r = sess.post(url=url)
+            if r.status_code == 200:
+                j = r.json()
+                version = j['version']
+                if len(j['file_list']) > 0:
+                    file_list = j['file_list']
+        except Exception as e:
+            print(e)
 
+        self.version = version
+        self.file_list = file_list
 
-def get_file_list():
-    _, domain, file_list = load_config(config_file)
-    url = 'http://{}/{}'.format(domain, file_list)
-    file_list = []
-    try:
-        sess = requests.Session()
-        r = sess.post(url=url)
-        if r.status_code == 200:
-            j = r.json()
-            if len(j['file_list']) > 0:
-                file_list = j['file_list']
-    except Exception as e:
-        print(e)
+    def get_newest_version(self):
+        return self.version
 
-    return file_list
-
-
-def update_or_not():
-    local_version, _, _ = load_config(config_file)
-    newest_version = get_newest_version()
-    return True if local_version < newest_version else False
+    def get_file_list(self):
+        return self.file_list
 
 
 def download_files(file_list):
     def download_big_file(file_name):
         try:
-            _, domain, _ = load_config(config_file)
             url = 'http://{}/{}'.format(domain, file_name)
             r = requests.get(url, stream=True)
             with open(file_name, "wb") as pdf:
@@ -78,16 +63,19 @@ def download_files(file_list):
 
 
 def run():
-    fl = get_file_list()
+    file_list = update_json.get_file_list()
 
-    if download_files(fl):
+    if download_files(file_list):
         config = ConfigParser()
         config.read(config_file)
-        config.set('update_config', 'version', get_newest_version())
+        config.set('update_config', 'version', update_json.get_newest_version())
         with open(config_file, 'w') as cf:
             config.write(cf)
 
 
 if __name__ == "__main__":
-    if update_or_not():
+    update_json = Update()
+    newest_version = update_json.get_newest_version()
+
+    if local_version < newest_version:
         run()
